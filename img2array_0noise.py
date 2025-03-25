1fa8a8d45c57e7752b4e5acbd22b5f569de3aec1
NEGRESC0 <156157000+NEGRESC0@users.noreply.github.com>
2025-03-25 11:32:33 -0300

    update 0 noise

--- img2array_0noise.py
+++ img2array_0noise.py
@@ -1,33 +1,44 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt #Para mostrar a imagem
+import matplotlib.pyplot as plt
 
-#Mostra cada matriz de pixel da imagem - CUIDADO: PODE SER MUITO GRANDE
+def moving_average(signal, window_size):
+    window = np.ones(int(window_size)) / float(window_size)
+    return np.convolve(signal, window, 'valid')
+
+# Mostra cada matriz de pixel da imagem - CUIDADO: PODE SER MUITO GRANDE
 np.set_printoptions(threshold=np.inf)
 
-#Carrega a imagem
+# Carrega a imagem
 img = cv2.imread(r'imgs\threepersons.jpeg')
 
-#Converte a imagem para escala de cinza
+# Converte a imagem para escala de cinza
 cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#Converte a imagem para array
-a = np.asarray(cinza)#USA O FORMATO BGR
+# Aplica um filtro Gaussiano para suavizar a imagem
+cinza_suave = cv2.GaussianBlur(cinza, (5, 5), 0)
 
-#Imprime a imagem em array
+# Converte a imagem suavizada para array
+a = np.asarray(cinza_suave)
+
+# Imprime a imagem em array
 #print(a)
 
-#Imprime o tamanho da imagem
+# Imprime o tamanho da imagem
 print(a.shape)
 
 # Calcula a soma das intensidades de pixels em cada coluna
 col_sum = np.sum(a, axis=0)
+
+# Aplica um filtro de média móvel ao sinal
+col_sum_smooth = moving_average(col_sum, window_size=10)
+
 plt.figure(figsize=(12, 6))
 
 # Primeiro subplot: exibe a imagem em escala de cinza
 plt.subplot(1, 2, 1)
 plt.title('Imagem em Escala de Cinza')
-plt.imshow(cinza, cmap='gray')
+plt.imshow(cinza_suave, cmap='gray')
 plt.axis('off')  # Oculta os eixos
 
 # Segundo subplot: exibe o gráfico das somas das intensidades de pixels por coluna
@@ -35,7 +46,7 @@ plt.subplot(1, 2, 2)
 plt.title('da esquerda para a direita')
 plt.xlabel('Coluna')
 plt.ylabel('Nível de claridade')
-plt.plot(col_sum)
+plt.plot(col_sum_smooth)
 
 # Exibe a figura com os dois subplots
 plt.tight_layout()
